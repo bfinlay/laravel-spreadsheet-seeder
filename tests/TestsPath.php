0b6d0adbc1f85e1d6670a24ac3f8f02abb0fd862
Brion Finlay <brion.finlay@gmail.com>
2024-08-15 22:53:42 -0500

    refactor tests structure eliminate tests-data package dependency

---
+++ tests/TestsPath.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace bfinlay\SpreadsheetSeeder\Tests;
+
+class TestsPath
+{
+    public static function relative($path = null)
+    {
+        // /.../laravel-excel-seeder/vendor/orchestra/testbench-core/laravel
+        $up = '../../../../tests';
+        return $up . ($path ? '/' . $path : '');
+    }
+
+    public static function forSettings($path = null)
+    {
+        return self::relative($path);
+    }
+
+    public static function absolute($path = null)
+    {
+        return $path ? __DIR__ . '/' . $path : __DIR__;
+    }
+
+    public static function forFinder($path = null)
+    {
+        return self::absolute($path);
+    }
+}
